e6612228a7a272d8057fa86f522303c897f3be55
andyjamesdavies <andy.davies@gmail.com>
2012-06-28 21:10:35 +0100

    refactoring the loader module from being a singleton to a constructor class

--- assets/js/application.js
+++ assets/js/application.js
@@ -8,12 +8,21 @@ define([
 		return {
 			initialize: function () {	
 
-				LoaderModule.onaudioloaded(function (audio) {					
-//					audio.play();
+				var loader1 = new LoaderModule();
+				loader1.onaudioloaded(function (audio) {					
+					audio.play();
 				});
 				
-				var fileInput = document.getElementById('fileInput');
-				fileInput.addEventListener("change", LoaderModule.handleFiles, false);
+				var fileInput1 = document.getElementById('fileInput1');
+				fileInput1.addEventListener("change", loader1.handleFiles, false);
+				
+				var loader2 = new LoaderModule();
+				loader2.onaudioloaded(function (audio) {					
+					audio.play();
+				});
+				
+				var fileInput2 = document.getElementById('fileInput2');
+				fileInput2.addEventListener("change", loader2.handleFiles, false);
 				
 			}
 		};
--- assets/js/modules/loader.module.js
+++ assets/js/modules/loader.module.js
@@ -3,40 +3,43 @@ define([
 	],
 	function ($) {
 		"use strict";
+
+		return function () {	
+			var audio = null,
+				onaudioloadedCallback = null;
 		
-		var audio = null,
-			onaudioloadedCallback = null;
 		
-		function _readFile(file, _callback) {
-			var reader = new FileReader();
-			reader.onloadend = function (e) {
-				if (e.target.readyState == FileReader.DONE) {
-					_callback(e);
+			function _readFile (file, _callback) {
+				var reader = new FileReader();
+				reader.onloadend = function (e) {
+					if (e.target.readyState == FileReader.DONE) {
+						_callback(e);
+					}
 				}
+				reader.readAsDataURL(file);
 			}
-			reader.readAsDataURL(file);
-		}
 		
-		function _setAudio(dataUrl) {
-			audio = document.createElement('audio');
-			audio.src = dataUrl;
-			
-			onaudioloadedCallback(audio);
-		}
+			function _setAudio (dataUrl) {
+				audio = document.createElement('audio');
+				audio.src = dataUrl;
+				
+				onaudioloadedCallback(audio);
+			}
 		
-		function _loadAudio(file) {
-			_readFile(file, function (e) {
-				_setAudio(e.target.result);
-			});	
-		}
+			function _loadAudio (file) {
+				_readFile(file, function (e) {
+					_setAudio(e.target.result);
+				});
+			}
 		
-		return {
-			onaudioloaded: function (_callback) {
+			this.onaudioloaded = function (_callback) {
 				onaudioloadedCallback = _callback;
-			},
-			handleFiles: function () {
-				_loadAudio(this.files[0]);
 			}
-		};
+		
+			this.handleFiles = function () {
+				_loadAudio(this.files[0]);
+			} 
+		
+		}
 	}
 );
